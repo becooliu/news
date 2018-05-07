114ee81e8e238b94696eed220912466dfe97f35e
luca.liu <243257686@qq.com>
2018-05-07 17:56:21 +0800

    [新增] 第一次上传

---
+++ src/App.vue
@@ -0,0 +1,102 @@
+<template>
+  <div id="app">
+      
+    <!-- <div class="tab">
+          <div class="tab-header">
+              <ul class="tab-list clearfix">
+                  <li v-for="item in tabList" class="list-item active" @click="checkFlagVal(index)"><span>精选<em class="coupon-num">(0)</em></span></li>
+                  <li class="list-item"><span>话题<em class="coupon-num">(0)</em></span></li>
+                  <li class="list-item"><span>作者<em class="coupon-num">(0)</em></span></li>
+              </ul>
+          </div>
+          <div class="tab-content-box">
+              <div class="content-item">
+    <img class="no-data" src="images/06.img1.png" alt="">
+  </div>
+              <div class="content-item">2</div>
+              <div class="content-item">3</div>
+              <div class="content-item">4</div>
+              <div class="content-item">5</div>    
+          </div>
+      </div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return {"activeFlag": -1}
+  },
+  methods: {
+    checkFlagVal(index){
+      this.activeFlag = index;
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@mixin box-sizing {
+	-webkit-box-sizing: border-box;
+	-moz-box-sizing: border-box;
+	box-sizing: border-box;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  
+}
+ul,
+li {
+  list-style: none;
+}
+.tab {
+    font-family: "Microsoft Yahei";
+    .tab-header {
+        height: 36px;
+        line-height: 36px;
+        margin-left: -10px;
+        margin-right: -10px;
+        .tab-list {
+            display: flex;
+            justify-content: space-around;
+            align-items: flex-start;
+            background-color: #fff;
+            overflow: hidden;
+            li {
+                text-align: center;
+                flex-grow: 2;
+                
+                span {
+                    display: inline-block;
+                    width: 100%;
+                    border-bottom: 2px solid transparent;
+                }
+                &.active {
+                    span {
+                        @include box-sizing;
+                        border-bottom-color: #ff1435;
+                    }
+                }
+            }
+        }
+    }
+    .tab-content-box {
+        margin-top: 2px;
+        .content-item {
+            display: none;
+            position: relative;
+            height: 100%;
+            min-height: 100%;
+            &:first-child {
+                display: block;
+            }
+            img.no-data {
+                
+                width: 100%;
+                max-width: 100%;
+            }
+        }
+    }
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,30 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import VueResource from 'vue-resource'
+
+Vue.config.productionTip = false
+Vue.use(VueResource)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>',
+  data(){
+    return {
+      tabList: ''
+    }
+  },
+  mounted: function(){
+    this.$nextTick(function(){
+      this.$http.get('/static/tabData.json').then(function(response){
+        this.tabList = response.data;
+        console.log(this.tabList);
+      })
+    })
+  }
+})
